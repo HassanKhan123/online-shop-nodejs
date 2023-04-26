12042f49955699409f8a554bc9e569efcac5e2fd
HassanKhan123 <hk2602809@gmail.com>
2023-04-26 10:37:02 +0500

    basic setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const { routes } = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
