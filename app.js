fab4c5611de8d7f64fcb59b14a0a5cc79aff0ef2
HassanKhan123 <hk2602809@gmail.com>
2023-04-29 12:59:54 +0500

    added: ejs templating engine

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require("path");
-const { engine } = require("express-handlebars");
 
 const { routes } = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine("hbs", engine({ defaultLayout: "main-layout", extname: "hbs" }));
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: false }));
@@ -18,7 +16,7 @@ app.use("/admin", routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { path: "" });
 });
 
 app.listen(3000, () => {
